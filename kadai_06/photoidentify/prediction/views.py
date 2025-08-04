ca0f29ee10834f8487a23555a1795c99317210d9
walnut025 <walnut25@outlook.jp>
2025-08-05 04:56:15 +0900

    Add files via upload

---
+++ kadai_06/photoidentify/prediction/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from .forms import ImageUploadForm
+from PIL import Image
+import numpy as np
+from keras.applications.vgg16 import preprocess_input
+from keras.applications.vgg16 import decode_predictions
+from django.conf import settings
+from keras.models import load_model
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from io import BytesIO
+import os
+
+def predict(request):
+    if request.method == 'GET':
+        form = ImageUploadForm()
+        return render(request, 'home.html', {'form': form})
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            img_file = form.cleaned_data['image']
+            img_file = BytesIO(img_file.read())
+            img = load_img(img_file, target_size=(224, 224))
+            img_array = img_to_array(img)
+            img_array = img_array.reshape((1, 224, 224, 3))
+            img_array = preprocess_input(img_array)
+
+            model_path = os.path.join(settings.BASE_DIR, 'prediction', 'models', 'vgg16.h5')
+            model = load_model(model_path)
+            result = model.predict(img_array)
+            prediction = decode_predictions(result)[0]
+            for pred in prediction:
+                print(f"カテゴリ: {pred[1]}, 確率: {pred[2]:.2f}")
+
+            img_data = request.POST.get('img_data')
+            return render(request, 'home.html', {'form': form, 'prediction': prediction, 'img_data': img_data})
+        else:
+            form = ImageUploadForm()
+            return render(request, 'home.html', {'form': form})
+
